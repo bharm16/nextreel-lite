ec31243298e4702ee363d606fbe8da17289c1ccd
Bryce Harmon <bph40@txstate.edu>
2023-11-03 16:35:00 -0500

    html working

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from scripts.set_filters_for_nextreel_backend import ImdbRandomMovieFetcher, ext
 from scripts.sort_and_filter import get_filtered_watched_movies, sort_movies
 from scripts.tmdb_data import get_backdrop_image_for_home
 
-
 app = Flask(__name__)
 app.secret_key = 'some_random_secret_key'  # IMPORTANT: Change this in production
 
@@ -26,13 +25,6 @@ default_backdrop_url = get_backdrop_image_for_home(default_movie_tmdb_id)
 def inject_default_backdrop_url():
     return dict(default_backdrop_url=default_backdrop_url)
 
-
-# Initialize Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Variable to determine whether a user should be logged out when the home page loads
-should_logout_on_home_load = True
 
 # Define global variables to hold the movie fetcher and criteria
 global_movie_fetcher = ImdbRandomMovieFetcher(db_config)
@@ -74,7 +66,7 @@ def fetch_and_render_movie(movie_queue, current_displayed_movie, previous_movies
     # Render the movie template, also passing the length of previous_movies_stack for UI control
     return render_template('movie.html',
                            movie=current_movie_data,
-                           current_user=current_user,
+
                            previous_count=len(previous_movies_stack))
 
 
@@ -82,6 +74,33 @@ def fetch_and_render_movie(movie_queue, current_displayed_movie, previous_movies
 def movie():
     global movie_queue, current_displayed_movie, previous_movies_stack  # Declare global variables
     return fetch_and_render_movie(movie_queue, current_displayed_movie, previous_movies_stack)
+
+
+@app.route('/next_movie', methods=['GET', 'POST'])
+def next_movie():
+    global current_displayed_movie  # Declare global variables
+
+    # Append the current displayed movie to the previous_movies_stack
+    if current_displayed_movie is not None:
+        previous_movies_stack.append(current_displayed_movie)
+
+    next_movie_data = None
+
+    # Check if future_movies_stack has any movies to go forward to
+    if future_movies_stack:
+        next_movie_data = future_movies_stack.pop()
+    else:
+        # If no future movies, get a new movie from the queue
+        next_movie_data = movie_queue.get()
+
+    # Update the current displayed movie
+    current_displayed_movie = next_movie_data
+
+    # Render the movie template, also passing the length of previous_movies_stack for UI control
+    return render_template('movie.html',
+                           movie=next_movie_data,
+                           current_user=current_user,
+                           previous_count=len(previous_movies_stack))
 
 
 @app.route('/filtered_movie', methods=['POST'])
@@ -113,69 +132,6 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/account_settings')
-@login_required
-def account_settings():
-    print("Current user's email:", current_user.email)
-
-    # Render the account settings template
-    return render_template('user_account_settings.html')
-
-
-
-
-@app.route('/watched_movies')
-@login_required
-def watched_movies():
-    # Initialize Account object with current user's details
-    current_account = Account(id=current_user.id, username=current_user.username, email=current_user.email)
-
-    # Fetch all watched movie details for the current user
-    watched_movie_details = current_account.get_watched_movies_by_user(current_account.id)
-
-    imdb_score_min = request.args.get('imdb_score_min', default=None, type=float)
-    imdb_score_max = request.args.get('imdb_score_max', default=None, type=float)
-
-    num_votes_min = request.args.get('num_votes_min', default=None, type=int)
-
-    genres = request.args.getlist('genres[]')  # Fetch multiple genre options
-    language = request.args.get('selectedLanguage', default=None, type=str)
-
-    # Fetch all watched movie details for the current user with filters
-    watched_movie_details = get_filtered_watched_movies(
-        user_db_config,
-        current_user.id,
-        imdb_score_min=imdb_score_min,
-        imdb_score_max=imdb_score_max,
-
-        num_votes_min=num_votes_min,
-        genres=genres,
-        language=language
-    )
-
-    # Get the sorting criteria from query parameters
-    sort_by = request.args.get('sort_by', default='tconst', type=str)
-
-    # Sort the movies using the imported function
-    watched_movie_details = sort_movies(watched_movie_details, sort_by)
-
-    if watched_movie_details is None or len(watched_movie_details) == 0:
-        flash("No watched movies found for this user.")
-
-    return render_template('watched_movies.html', watched_movie_details=watched_movie_details)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    # Query to fetch user details from the database
-    # user_data = execute_query(user_db_config, "SELECT * FROM users WHERE id=%s", (user_id,))
-    user_data = get_user_by_id(user_id)
-    # If user data exists, return a User object
-    if user_data:
-        return Account(id=user_data['id'], username=user_data['username'], email=user_data['email'])  # Add email here
-    # Otherwise, return None
-
-
 # Declare a global variable to store the last displayed movie
 global last_displayed_movie
 
@@ -197,105 +153,7 @@ def previous_movie():
     # Render the movie template, also passing the length of previous_movies_stack for UI control
     return render_template('movie.html',
                            movie=previous_movie_data,
-                           current_user=current_user,
                            previous_count=len(previous_movies_stack))
-
-
-@app.route('/next_movie', methods=['GET', 'POST'])
-def next_movie():
-    global current_displayed_movie  # Declare global variables
-
-    # Append the current displayed movie to the previous_movies_stack
-    if current_displayed_movie is not None:
-        previous_movies_stack.append(current_displayed_movie)
-
-    next_movie_data = None
-
-    # Check if future_movies_stack has any movies to go forward to
-    if future_movies_stack:
-        next_movie_data = future_movies_stack.pop()
-    else:
-        # If no future movies, get a new movie from the queue
-        next_movie_data = movie_queue.get()
-
-    # Update the current displayed movie
-    current_displayed_movie = next_movie_data
-
-    # Render the movie template, also passing the length of previous_movies_stack for UI control
-    return render_template('movie.html',
-                           movie=next_movie_data,
-                           current_user=current_user,
-                           previous_count=len(previous_movies_stack))
-
-
-# Assuming current_user is an instance of Account
-@app.route('/seen_it', methods=['POST'])
-@login_required
-def seen_it():
-    global current_displayed_movie
-    if current_displayed_movie:
-        tconst = current_displayed_movie.get("imdb_id")
-        current_user.log_movie_to_user_account(current_user.id, current_user.username, tconst, current_displayed_movie,
-                                               user_db_config)
-        return redirect(url_for('movie'))
-    else:
-        return jsonify({'status': 'failure', 'message': 'No movies in the queue'}), 400
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Call the class method, passing in db_config explicitly
-        user_data = Account.login_user(username, password, user_db_config)
-
-        if user_data:
-            # Create an Account instance for this user
-            user = Account(id=user_data['id'], username=user_data['username'], email=user_data['email'])
-            login_user(user)
-            return redirect(url_for('home'))
-        else:
-            flash("Invalid username or password")
-
-    return render_template('user_login.html')
-
-
-# Route for logout
-@app.route('/logout')
-@login_required  # Require the user to be logged in to access this route
-def logout():
-    # Log the user out
-    logout_user()
-    # Redirect to the login page
-    return redirect(url_for('login'))
-
-
-# Route for user registration
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-
-        result = Account.register_user(username, email, password, user_db_config)
-
-        if result == "Username already exists.":
-            flash("Username already exists.")
-            return render_template('create_account_form.html')
-
-        flash("ShowModal")
-        return redirect(url_for('login'))
-
-    return render_template('create_account_form.html')
 
 
 # Route for setting filters
@@ -310,29 +168,6 @@ def set_filters():
 
     # Render the filter settings template
     return render_template('set_filters.html')
-
-
-@app.route('/add_to_watchlist', methods=['POST'])
-@login_required
-def add_to_watchlist():
-    global current_displayed_movie  # Consider using a different state management approach
-    if current_displayed_movie:
-        tconst = current_displayed_movie.get("imdb_id")
-
-        # Assuming current_user is an instance of Account
-        current_user.add_movie_to_watchlist(current_user.id, current_user.username, tconst, current_displayed_movie,
-                                            user_db_config)
-
-        return redirect(url_for('movie'))
-    else:
-        return jsonify({'status': 'failure', 'message': 'No movies in the queue'}), 400
-
-
-@app.route('/user_watch_list')
-@login_required
-def user_watch_list():
-    watchlist_movies = get_all_movies_in_watchlist(current_user.id)
-    return render_template('user_watchlist.html', watchlist_movies=watchlist_movies)
 
 
 if __name__ == "__main__":
