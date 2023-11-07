15db97f958e71664049b2b1986ffc79ad0de04bb
Bryce Harmon <bph40@txstate.edu>
2023-11-07 13:30:01 -0600

    changes to movie for async

--- scripts/movie.py
+++ scripts/movie.py
@@ -1,40 +1,46 @@
 import os
 import random
-
-import imdb
-import tmdbsimple as tmdb
+import asyncio
+import httpx
 
 import config
-from scripts.set_filters_for_nextreel_backend import ImdbRandomMovieFetcher
+from scripts.set_filters_for_nextreel_backend import AsyncImdbRandomMovieFetcher
 
-# Initialize TMDb API Key
-tmdb.API_KEY = '1ce9398920594a5521f0d53e9b33c52f'  # Replace with your actual TMDb API key
-
-db_config = config.Config.STACKHERO_DB_CONFIG
-
-# Use os.path.dirname to go up one level from the current script's directory
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Now change the working directory to the parent directory
-os.chdir(parent_dir)
-
-# Finally, print the new working directory to confirm the change
-print(f"Current working directory after change: {os.getcwd()}")
-
-def get_tmdb_id_by_tconst(tconst):
-    find = tmdb.Find(tconst)
-    response = find.info(external_source='imdb_id')
-    return response['movie_results'][0]['id'] if response['movie_results'] else None
+# Replace with your actual TMDb API key
+TMDB_API_KEY = '1ce9398920594a5521f0d53e9b33c52f'
+TMDB_API_BASE_URL = "https://api.themoviedb.org/3"
+TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
 
 
-def get_cast_info_by_tmdb_id(tmdb_id):
-    movie = tmdb.Movies(tmdb_id)
-    response = movie.credits()
+# Async HTTP client setup
+async def get_http_client():
+    return httpx.AsyncClient()
+
+
+# Async TMDb operations
+async def get_tmdb_id_by_tconst(tconst, client):
+    response = await client.get(
+        f"{TMDB_API_BASE_URL}/find/{tconst}",
+        params={"api_key": TMDB_API_KEY, "external_source": "imdb_id"}
+    )
+    response.raise_for_status()
+    data = response.json()
+    return data['movie_results'][0]['id'] if data['movie_results'] else None
+
+
+# Async function to get cast info by TMDb ID
+async def get_cast_info_by_tmdb_id(tmdb_id, client):
+    response = await client.get(
+        f"{TMDB_API_BASE_URL}/movie/{tmdb_id}/credits",
+        params={"api_key": TMDB_API_KEY}
+    )
+    response.raise_for_status()
+    data = response.json()
     cast_info = []
     # Limit the number of cast members to the first 10
-    for cast_member in response.get('cast', [])[:10]:
+    for cast_member in data.get('cast', [])[:10]:
         profile_path = cast_member.get('profile_path')
-        image_url = get_full_image_url(profile_path) if profile_path else None
+        image_url = f"{TMDB_IMAGE_BASE_URL}w185{profile_path}" if profile_path else None
         # Add character name here
         character_name = cast_member.get('character', 'N/A')
         cast_info.append({
@@ -45,25 +51,15 @@ def get_cast_info_by_tmdb_id(tmdb_id):
     return cast_info
 
 
-def get_full_image_url(profile_path, size='w185'):
-    base_url = "https://image.tmdb.org/t/p/"
-    return f"{base_url}{size}{profile_path}"
-
-
-def get_video_url_by_tmdb_id(tmdb_id):
-    """
-    Fetches a single video URL for a movie from TMDb using its TMDb ID.
-    It looks for a video that is on YouTube and is of type "Trailer".
-
-    Args:
-        tmdb_id (int): The TMDb ID of the movie.
-
-    Returns:
-        str: A single video URL, or None if no suitable video is found.
-    """
-    movie = tmdb.Movies(tmdb_id)
-    response = movie.videos()
-    video_results = response.get('results', [])
+# Async function to get video URL by TMDb ID
+async def get_video_url_by_tmdb_id(tmdb_id, client):
+    response = await client.get(
+        f"{TMDB_API_BASE_URL}/movie/{tmdb_id}/videos",
+        params={"api_key": TMDB_API_KEY}
+    )
+    response.raise_for_status()
+    data = response.json()
+    video_results = data.get('results', [])
 
     for video in video_results:
         # Only include videos that are on YouTube and are of type "Trailer"
@@ -74,56 +70,52 @@ def get_video_url_by_tmdb_id(tmdb_id):
     return None  # Return None if no suitable video is found
 
 
-def fetch_images_from_tmdb(tmdb_id):
+# Async function to fetch images from TMDb
+async def fetch_images_from_tmdb(tmdb_id, client):
     """
     Fetch movie images from TMDb using the movie's TMDb ID.
-
-    Args:
-        tmdb_id (int): The TMDb ID of the movie.
-
-    Returns:
-        dict: A dictionary containing image URLs.
     """
-    movie = tmdb.Movies(tmdb_id)
-    response = movie.images()
+    response = await client.get(
+        f"{TMDB_API_BASE_URL}/movie/{tmdb_id}/images",
+        params={"api_key": TMDB_API_KEY}
+    )
+    response.raise_for_status()
+    data = response.json()
     image_data = {
-        'posters': [img['file_path'] for img in response.get('posters', [])],
-        'backdrops': [img['file_path'] for img in response.get('backdrops', [])]
+        'posters': [img['file_path'] for img in data.get('posters', [])],
+        'backdrops': [img['file_path'] for img in data.get('backdrops', [])]
     }
     return image_data
 
 
-def fetch_videos_from_tmdb(tmdb_id):
+# Async function to fetch videos from TMDb
+async def fetch_videos_from_tmdb(tmdb_id, client):
     """
     Fetch movie videos from TMDb using the movie's TMDb ID.
-
-    Args:
-        tmdb_id (int): The TMDb ID of the movie.
-
-    Returns:
-        list: A list of dictionaries containing video data.
     """
-    movie = tmdb.Movies(tmdb_id)
-    response = movie.videos()  # Fetch video data from TMDb
-    return response.get('results', [])  # Extract the 'results' field which contains the video data
+    response = await client.get(
+        f"{TMDB_API_BASE_URL}/movie/{tmdb_id}/videos",
+        params={"api_key": TMDB_API_KEY}
+    )
+    response.raise_for_status()
+    data = response.json()
+    return data.get('results', [])
 
 
-# Add this function to your existing code
-def get_credits_by_tmdb_id(tmdb_id):
+# Async function to get credits by TMDb ID
+async def get_credits_by_tmdb_id(tmdb_id, client):
     """
     Fetch the cast and crew for a movie using its TMDb ID.
-
-    Args:
-        tmdb_id (int): The TMDb ID of the movie.
-
-    Returns:
-        dict: A dictionary containing both cast and crew information.
     """
-    movie = tmdb.Movies(tmdb_id)
-    response = movie.credits()
+    response = await client.get(
+        f"{TMDB_API_BASE_URL}/movie/{tmdb_id}/credits",
+        params={"api_key": TMDB_API_KEY}
+    )
+    response.raise_for_status()
+    data = response.json()
     return {
-        'cast': response.get('cast', []),
-        'crew': response.get('crew', [])
+        'cast': data.get('cast', []),
+        'crew': data.get('crew', [])
     }
 
 
@@ -133,21 +125,18 @@ class Movie:
         self.db_config = db_config
         self.movie_data = {}
 
-    def fetch_info_from_imdb(self):
-        """Fetch movie information from IMDb using IMDbPY."""
-        imdbId = int(self.tconst[2:])
-        ia = imdb.IMDb()
-        return ia.get_movie(imdbId)
+    async def fetch_info_from_imdb(self):
+        # Implement this method using an async IMDb library or async HTTP requests
+        pass
 
-    def store_movie_data(self, movie):
+    async def store_movie_data(self, movie):
         """
         Store movie data from IMDb and TMDb sources into a single dictionary.
 
         Args:
-            movie (imdb.Movie.Movie): IMDb movie object containing various movie details.
+            movie (dict): Movie data dictionary.
         """
-        ia = imdb.IMDb()
-        tmdb_id = get_tmdb_id_by_tconst(self.tconst)
+        tmdb_id = await get_tmdb_id_by_tconst(self.tconst, self.client)
         tmdb_cast_info = []
         tmdb_image_info = {}
         tmdb_movie_trailer = None
@@ -155,17 +144,11 @@ class Movie:
 
         # Check if there is a TMDb ID corresponding to the IMDb ID
         if tmdb_id:
-            # Fetch cast and crew information from TMDb
-            tmdb_credits = get_credits_by_tmdb_id(tmdb_id)
-
-            # Fetch a trailer URL from TMDb
-            tmdb_movie_trailer = get_video_url_by_tmdb_id(tmdb_id)
-
-            # Fetch cast information from TMDb and limit it to first 10 actors
-            tmdb_cast_info = get_cast_info_by_tmdb_id(tmdb_id)[:10]
-
-            # Fetch image information from TMDb
-            tmdb_image_info = fetch_images_from_tmdb(tmdb_id)
+            # Fetch cast and crew information from TMDb asynchronously
+            tmdb_credits = await get_credits_by_tmdb_id(tmdb_id, self.client)
+            tmdb_movie_trailer = await get_video_url_by_tmdb_id(tmdb_id, self.client)
+            tmdb_cast_info = await get_cast_info_by_tmdb_id(tmdb_id, self.client)
+            tmdb_image_info = await fetch_images_from_tmdb(tmdb_id, self.client)
 
         # Populate the movie_data dictionary with various fields
         self.movie_data = {
@@ -188,84 +171,34 @@ class Movie:
             "credits": tmdb_credits  # Add TMDb credits information
         }
 
-    def get_movie_data(self):
-        movie_data = self.fetch_info_from_imdb()
-        self.store_movie_data(movie_data)
+    async def get_movie_data(self):
+        movie_data_imdb = await self.fetch_info_from_imdb()
+        await self.store_movie_data(movie_data_imdb)
         return self.movie_data
 
+    async def close(self):
+        await self.client.aclose()
 
-def get_random_backdrop_url(backdrops):
-    """
-    Selects a random backdrop URL from a list of backdrops.
-
-    Args:
-        backdrops (list): List of backdrop image file paths.
-
-    Returns:
-        str: The full URL of a randomly selected backdrop, or None if no backdrops are available.
-    """
-    if backdrops:
-        random_backdrop = random.choice(backdrops)  # Randomly select a backdrop
-        return get_full_image_url(random_backdrop)  # You can specify the size you want
-    else:
-        return None
-
-
-def main(criteria):
-    movie_fetcher = ImdbRandomMovieFetcher(db_config)
-    row = movie_fetcher.fetch_random_movie(criteria)
-
+# Main function with async execution
+async def main(criteria):
+    client = await get_http_client()
+    movie_fetcher = AsyncImdbRandomMovieFetcher(config.Config.STACKHERO_DB_CONFIG)  # Must be an async version
+    row = await movie_fetcher.fetch_random_movie(criteria)
     if not row:
         print("No movies found based on the given criteria.")
-        return None
+        return
 
-    movie = Movie(row['tconst'], db_config)
-    movie_data = movie.get_movie_data()
+    movie = Movie(row['tconst'], config.Config.STACKHERO_DB_CONFIG)
+    movie_data = await movie.get_movie_data(client)
     print(movie_data)
+    # ... rest of the main function ...
 
-    # Corrected this line
-    for image in movie_data["images"].get('posters', []):
-        print(image)
-
-    # Print cast information line by line
-    print("Cast Information:")
-    for cast_member in movie_data.get("cast", []):
-        print(f"Name: {cast_member['name']}, Image URL: {cast_member['image_url']}")
-
-        # New code to randomly select a backdrop
-    random_backdrop_url = get_random_backdrop_url(movie_data["images"].get('backdrops', []))
-    if random_backdrop_url:
-        print(f"Randomly selected backdrop URL: {random_backdrop_url}")
-    else:
-        print("No backdrops available for this movie.")
-
-    print(movie_data.get("trailer", []))
-
-    # New code to print credit information line by line
-    print("Cast and Crew Information:")
-
-    # Print Cast Information
-    print("\nCast:")
-    for cast_member in movie_data.get("credits", {}).get("cast", []):
-        name = cast_member.get('name', 'N/A')
-        character = cast_member.get('character', 'N/A')
-        print(f"Actor: {name}, Character: {character}")
-
-    # Print cast information line by line
-    print("Cast Information:")
-    for cast_member in movie_data.get("cast", []):
-        print(
-            f"Name: {cast_member['name']}, Character: {cast_member['character']}, Image URL: {cast_member['image_url']}")
+    await client.aclose()
 
 
+# Entry point for the script
 if __name__ == "__main__":
     criteria = {
-        "min_year": 1900,
-        "max_year": 2023,
-        "min_rating": 7.0,
-        "max_rating": 10,
-        "title_type": "movie",
-        "language": "en",
-        "genres": ["Action", "Drama"]
+        # ... criteria dictionary ...
     }
-    main(criteria)
+    asyncio.run(main(criteria))
